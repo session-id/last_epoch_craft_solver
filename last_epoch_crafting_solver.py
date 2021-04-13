c4ca69ec9d07ec3f6c46169f82ecc77947e9965f
Vinson Luo <vinsonluo0@gmail.com>
2021-04-12 20:51:36 -0700

    Minor changes

--- last_epoch_crafting_solver.py
+++ last_epoch_crafting_solver.py
@@ -3,9 +3,9 @@ import itertools
 import os
 import pickle
 
-starting_instability = 0
-starting_tiers = [2, 2, 0, 0]
-tiers_to_beat = [5, 5, 1, 1]
+starting_instability = 5
+starting_tiers = [5, 0, 0, 0]
+tiers_to_beat = [5, 5, 5, 5]
 
 MAX_TIER = 5
 MAX_INSTABILITY = 100
@@ -16,13 +16,14 @@ target_filename = os.path.join('craft_cache', '_'.join([str(x) for x in tiers_to
 def fracture_chance(instability, tier):
 	return (instability + 5 * tier if instability > 0 else 0) / 100
 
-if os.path.exists(target_filename):
+if False and os.path.exists(target_filename):
 	with open(target_filename, 'rb') as f:
 		print("Loading cache target file")
 		expected_payoff, best_actions = pickle.load(f)
 else:
 	# Do +5 below to account for possibility that itmes go above 5 instability
 	expected_payoff = np.zeros((MAX_INSTABILITY+5,) + (MAX_TIER + 1,) * 4)
+	best_glyph = np.zeros((MAX_INSTABILITY+5,) + (MAX_TIER + 1,) * 4, dtype=int)
 	best_actions = np.empty((MAX_INSTABILITY+5,) + (MAX_TIER + 1,) * 4, dtype='O')
 	slices = [slice(None)]
 	for tier_to_beat in tiers_to_beat:
@@ -39,21 +40,23 @@ else:
 			if expected_payoff[key] == 1:
 				continue
 
-			best_action = (0, "Nothing")
+			best_action = (0, 0, "Nothing")
 			for upgrade_position, tier in enumerate(tiers):
 				if tier == MAX_TIER:
 					continue
 				new_tiers = list(tiers)
 				new_tiers[upgrade_position] += 1
 				fchance = fracture_chance(instability, tier)
-				guardian_payoff = (1 - max(fchance - 0.25, 0)) * expected_payoff[(instability+5,) + tuple(new_tiers)]
-				stability_payoff = (1 - fchance) * np.mean(expected_payoff[(slice(instability+2, instability+6),) + tuple(new_tiers)])
-				guardian_action = (guardian_payoff, f'G {upgrade_position}')
-				stability_action = (stability_payoff, f'S {upgrade_position}')
+				guardian_payoff = max(1 - max(fchance - 0.25, 0), 0) * expected_payoff[(instability+5,) + tuple(new_tiers)]
+				stability_payoff = max(1 - fchance, 0) * np.mean(expected_payoff[(slice(instability+2, instability+6),) + tuple(new_tiers)])
+				guardian_action = (guardian_payoff, 1, f'G {upgrade_position}')
+				stability_action = (stability_payoff, 0, f'S {upgrade_position}')
 				best_action = max(best_action, guardian_action)
 				best_action = max(best_action, stability_action)
 
-			expected_payoff[key], best_actions[key] = best_action
+			if best_action[2][0] == 'S':
+				best_glyph[key] = 1
+			expected_payoff[key], _, best_actions[key] = best_action
 
 	with open(target_filename, 'wb') as f:
 		print("Storing cache target file")
